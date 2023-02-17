aae92b373f74ae931632ee54d140f29cf4e6bff5
LeafField <109466326+LeafField@users.noreply.github.com>
2023-02-17 22:56:51 +0900

    ユーザー認証機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 import Chat from "./components/chat/Chat";
+import Login from "./components/login/Login";
 import Sidebar from "./components/sidebar/Sidebar";
+import { login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
-      {/* chat */}
-      <Chat />
+      {user ? (
+        <>
+          {/* sidebar */}
+          <Sidebar />
+          {/* chat */}
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaG-6ub2zdV0iHtnw4a2n2iHrYhM5rnW0",
+  authDomain: "discode-clone-udemy.firebaseapp.com",
+  projectId: "discode-clone-udemy",
+  storageBucket: "discode-clone-udemy.appspot.com",
+  messagingSenderId: "8621228393",
+  appId: "1:8621228393:web:921f7692e31b505ba77df8",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider };
